f33545816e72bbcd97e13e1fb8708ac2fed3973d
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-02-06 17:22:14 +0900

    (feat: Controller) GET HTTP 엔드포인트 노출

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -1,9 +1,27 @@
 package com.optimagrowth.license.controller;
 
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
+    // 필드 주입
+    // 의존 관계가 눈에 보이지 않아 추상적임.
+    // 단일 책임 원칙 위반
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping(value = "/{licenseId}")
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
+                                              @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLicense(licenseId, organizationId);
+        return ResponseEntity.ok(license);
+    }
 }
