491dcec5e2a337b316dc049d60badfbae39ab0a3
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-02-07 23:52:31 +0900

    (feat: Controller) 라이선싱 서비스에 국제화 추가하기

--- src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -2,12 +2,39 @@ package com.optimagrowth.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class LicenseServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicenseServiceApplication.class, args);
+	}
+
+	// 애플리케이션을 다른 언어에 적응할 수 있도록 하는 필수 요구사항인 국제화 추가
+	// 메시지를 조회할 때 Locale을 설정하지 않으면 messageSource는 LocaleResolver로 설정된 디폴트 로케일을 사용.
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		// US를 기본 로케일로 설정한다.
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		// 메시지가 발견되지 않아도 에러를 던지지 않고 대신 메시지 코드를 반환한다.
+		messageSource.setUseCodeAsDefaultMessage(true);
+		// 언어 프로퍼티 파일의 기본 이름을 설정한다.
+		// 예를 들어 이탈리아에 있다면 'Locale.IT'를 사용하고 'messages_it.properties'라는 파일을 갖는다.
+		messageSource.setBasenames("messages");
+		return messageSource;
 	}
 
 }
--- src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,8 +1,12 @@
 package com.optimagrowth.license.service;
 
 import com.optimagrowth.license.model.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.util.Locale;
 import java.util.Random;
 
 @Service
@@ -23,12 +27,17 @@ public class LicenseService {
         return license;
     }
 
-    public String createLicense(License license, String organizationId) {
+    @Autowired
+    MessageSource messages;
+
+    public String createLicense(License license, String organizationId, Locale locale) {
         String responseMessage = null;
 
-        if (license != null) {
+        if (!StringUtils.isEmpty(license)) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the post and the object is: %s", license.toString());
+            responseMessage = String.format(
+                    messages.getMessage("license.create.message", null, locale), license.toString()
+            );
         }
 
         return responseMessage;
@@ -36,10 +45,11 @@ public class LicenseService {
 
     public String updateLicense(License license, String organizationId) {
         String responseMessage = null;
-        if (license != null) {
+        if (!StringUtils.isEmpty(license)) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the put and the object is: %s", license.toString());
+            responseMessage = String.format(messages.getMessage("license.update.message", null, null), license.toString());
         }
+        
         return responseMessage;
     }
 
