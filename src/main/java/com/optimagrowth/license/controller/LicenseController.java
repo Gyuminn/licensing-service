2f80631eb6ab494967357ef34b267f463cbf8604
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-02-08 00:22:58 +0900

    (feat: relative-link) 관련 링크를 표시하는 스프링 HATEOAS 구현

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
@@ -21,6 +24,22 @@ public class LicenseController {
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
                                               @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId);
+
+        // linkTo 메서드는 licenseController 클래스를 검사해서 루트 매핑을 얻고,
+        // methodOn() 메서드는 대상 메서드에 더미 호출을 수행하여 메서드 매핑을 가져온다.
+        // 이제 GET 호출을 하면 응답에서 컨트롤러 클래스에 대한 링크를 생성한다. (WebMvcLinkBuilder라는 유틸리티 클래스가 해줌.)
+        license.add(linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId()))
+                        .withRel("deleteLicense"));
         return ResponseEntity.ok(license);
     }
 
@@ -33,7 +52,7 @@ public class LicenseController {
     @PostMapping
     public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
                                                 @RequestBody License request,
-                                                @RequestHeader(value="Accept-Language", required=false) Locale locale) {
+                                                @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
         return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
     }
 
--- src/main/java/com/optimagrowth/license/model/License.java
+++ src/main/java/com/optimagrowth/license/model/License.java
@@ -3,11 +3,13 @@ package com.optimagrowth.license.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
 @ToString
-public class License {
+// RepresentationModel<License>는 License 모델 클래스에 링클르 추가할 수 있게 한다.
+public class License extends RepresentationModel<License> {
     private int id;
     private String licenseId;
     private String description;
